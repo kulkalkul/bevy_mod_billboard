8941f27161fc4b7b5db862cbd397775d6bd90b3d
Bora <yokunjon@gmail.com>
2023-03-08 02:38:23 +0300

    Add full text and texture example

---
+++ examples/text.rs
@@ -0,0 +1,43 @@
+use bevy::prelude::*;
+use bevy_mod_billboard::{BillboardPlugin, BillboardTextBundle};
+
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_plugin(BillboardPlugin)
+        .add_startup_system(setup_scene)
+        .run();
+}
+
+fn setup_scene(mut commands: Commands, asset_server: Res<AssetServer>) {
+    let fira_sans_regular_handle = asset_server.load("FiraSans-Regular.ttf");
+    commands
+        .spawn(BillboardTextBundle {
+            transform: Transform::from_scale(Vec3::splat(0.0085)),
+            text: Text::from_sections([
+                TextSection {
+                    value: "IMPORTANT".to_string(),
+                    style: TextStyle {
+                        font_size: 60.0,
+                        font: fira_sans_regular_handle.clone(),
+                        color: Color::ORANGE,
+                    }
+                },
+                TextSection {
+                    value: " text".to_string(),
+                    style: TextStyle {
+                        font_size: 60.0,
+                        font: fira_sans_regular_handle.clone(),
+                        color: Color::WHITE,
+                    }
+                }
+            ]).with_alignment(TextAlignment::CENTER),
+            ..default()
+        });
+
+    commands.spawn(Camera3dBundle {
+        transform: Transform::from_translation(Vec3::new(5., 0., 0.))
+            .looking_at(Vec3::ZERO, Vec3::Y),
+        ..default()
+    });
+}
---
+++ examples/texture.rs
@@ -0,0 +1,32 @@
+use bevy::prelude::*;
+use bevy::prelude::shape::Quad;
+use bevy_mod_billboard::{BillboardMeshHandle, BillboardPlugin, BillboardTexture, BillboardTextureBundle};
+
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_plugin(BillboardPlugin)
+        .add_startup_system(setup_scene)
+        .run();
+}
+
+fn setup_scene(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    mut billboard_textures: ResMut<Assets<BillboardTexture>>,
+    mut meshes: ResMut<Assets<Mesh>>,
+) {
+    let image_handle = asset_server.load("rust-logo-256x256.png");
+    commands
+        .spawn(BillboardTextureBundle {
+            texture: billboard_textures.add(BillboardTexture::Single(image_handle)),
+            mesh: BillboardMeshHandle(meshes.add(Quad::new(Vec2::new(2., 2.)).into())),
+            ..default()
+        });
+
+    commands.spawn(Camera3dBundle {
+        transform: Transform::from_translation(Vec3::new(5., 0., 0.))
+            .looking_at(Vec3::ZERO, Vec3::Y),
+        ..default()
+    });
+}
