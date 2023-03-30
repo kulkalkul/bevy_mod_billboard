91cf2a923bd013bca70d209bc6b0cddeca92dafd
Bora <yokunjon@gmail.com>
2023-03-30 11:57:35 +0300

    Add Y locking example

---
+++ examples/lock_y.rs
@@ -0,0 +1,47 @@
+use bevy::prelude::*;
+use bevy::prelude::shape::{Plane, Quad};
+use bevy_mod_billboard::{BillboardLockAxisBundle, BillboardMeshHandle, BillboardPlugin, BillboardTexture, BillboardTextureBundle};
+
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_plugin(BillboardPlugin)
+        .add_startup_system(setup_scene)
+        .run();
+}
+
+fn setup_scene(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    mut billboard_textures: ResMut<Assets<BillboardTexture>>,
+    mut meshes: ResMut<Assets<Mesh>>,
+) {
+    let image_handle = asset_server.load("tree.png");
+    commands
+        .spawn(BillboardLockAxisBundle {
+            billboard_bundle: BillboardTextureBundle {
+                transform: Transform::from_translation(Vec3::new(2.0, 2.0, 0.0)),
+                texture: billboard_textures.add(BillboardTexture::Single(image_handle.clone())),
+                mesh: BillboardMeshHandle(meshes.add(Quad::new(Vec2::new(2., 4.)).into())),
+                ..default()
+            },
+            ..default()
+        });
+    commands
+        .spawn(BillboardTextureBundle {
+            transform: Transform::from_translation(Vec3::new(-2.0, 2.0, 0.0)),
+            texture: billboard_textures.add(BillboardTexture::Single(image_handle)),
+            mesh: BillboardMeshHandle(meshes.add(Quad::new(Vec2::new(2., 4.)).into())),
+            ..default()
+        });
+    commands.spawn(PbrBundle {
+        mesh: meshes.add(Plane { size: 4.0, subdivisions: 0 }.into()),
+        ..default()
+    });
+
+    commands.spawn(Camera3dBundle {
+        transform: Transform::from_translation(Vec3::new(0., 15., 2.))
+            .looking_at(Vec3::ZERO, Vec3::Y),
+        ..default()
+    });
+}
