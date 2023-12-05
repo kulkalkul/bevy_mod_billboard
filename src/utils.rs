445a902e4d4efdbf323d73138816a4c1647990f1
Bora <yokunjon@gmail.com>
2023-12-05 10:16:22 +0300

    Fix propagation bug and introduce an example (#20)

--- src/utils.rs
+++ src/utils.rs
@@ -1,16 +1,21 @@
 use bevy::{
-    math::{Mat4, Vec4},
+    math::Mat4,
     transform::components::{GlobalTransform, Transform},
 };
 
 use crate::{pipeline::BillboardUniform, BillboardLockAxis};
 
-pub fn compute_matrix_without_rotation(transform: &Transform) -> Mat4 {
+// TODO: Maybe add scale as uniform to shader and do this in shader?
+pub fn compute_matrix_without_rotation(
+    global_transform: &GlobalTransform,
+    transform: &Transform,
+) -> Mat4 {
+    let global_matrix = global_transform.compute_matrix();
     Mat4::from_cols(
         Mat4::IDENTITY.x_axis * transform.scale.x,
         Mat4::IDENTITY.y_axis * transform.scale.y,
         Mat4::IDENTITY.z_axis * transform.scale.z,
-        Vec4::from((transform.translation, 1.0)),
+        global_matrix.w_axis,
     )
 }
 
@@ -22,7 +27,7 @@ pub fn calculate_billboard_uniform(
     let transform = if lock_axis.is_some() {
         global_transform.compute_matrix()
     } else {
-        compute_matrix_without_rotation(transform)
+        compute_matrix_without_rotation(global_transform, transform)
     };
 
     BillboardUniform { transform }
